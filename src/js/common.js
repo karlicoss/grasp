ff574ce2f32b106d9b0d8397c5ccfa7fc41f7b8e
Dima Gerasimov <karlicoss@gmail.com>
2019-01-28 23:41:15 +0000

    Fix notification in firefox, adapt manifest for chrome

--- src/js/common.js
+++ src/js/common.js
@@ -4,18 +4,11 @@ export const COMMAND_CAPTURE_SIMPLE = 'capture-simple';
 export const METHOD_CAPTURE_WITH_EXTRAS = 'captureWithExtras';
 
 export function showNotification(text: string, priority: number=0) {
-    if (Notification.permission !== "granted") {
-        Notification.requestPermission();
-    } else {
-        // TODO ugh. is there no way to show in-browser only notification??
-        new Notification(
-            'grasp-notification',
-            // $FlowFixMe
-            {
-                title: 'Grasp extension',
-                body: text,
-                priority: priority,
-            },
-        );
-    }
+    chrome.notifications.create({
+        'type': "basic",
+        'title': "org-grasp",
+        'message': text,
+        'priority': priority,
+        'iconUrl': 'unicorn.png',
+    });
 }
--- utils/env.js
+++ utils/env.js
@@ -3,4 +3,5 @@ module.exports = {
     NODE_ENV: (process.env.NODE_ENV || "development"),
     PORT: (process.env.PORT || 3000),
     ANY_HOST: (process.env.ANY_HOST ? true : false),
+    TARGET: (process.env.TARGET || 'chrome'),
 };
