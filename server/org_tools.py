c804ef7a7c3904c03af56006656fb9497c62b35e
Anish Lakhwara <anish+git@lakhwara.com>
2022-12-20 11:27:12 +1000

    remove \ from escaped % characters in string

--- server/org_tools.py
+++ server/org_tools.py
@@ -65,7 +65,7 @@ Additionally, supports :selection, :comment and :tags expansions.
 You can look at `test_templates` for some specific examples.
     """
     # py_template = re.sub(r'%[:]?([?\w]+)', r'{\1}', org_template)
-    py_template = re.sub(r'[^\\]%[:]?([?\w]+)', r'{\1}', org_template)
+    py_template = re.sub(r'[^\\]%[:]?([?\w]+)', r'{\1}', org_template).replace('\\%', '%')
 
     NOW = datetime.now()
     org_date = date2org(NOW)
