98af2ac311c982c823d5b627b976b9ab5b7c50ba
karlicoss <karlicoss@gmail.com>
2024-04-18 00:02:11 +0100

    extension: migrate background page to typescript

--- extension/src/common.ts
+++ extension/src/common.ts
@@ -1,4 +1,16 @@
+import browser from "webextension-polyfill"
+
 // NOTE: keep consistent with webpack.config.js
 export const COMMAND_CAPTURE_SIMPLE = 'capture-simple'
 
 export const METHOD_CAPTURE_WITH_EXTRAS = 'captureWithExtras'
+
+export function showNotification(text: string, priority: number=0) {
+    browser.notifications.create({
+        'type': "basic",
+        'title': "grasp",
+        'message': text,
+        'priority': priority,
+        'iconUrl': 'img/unicorn.png',
+    })
+}
